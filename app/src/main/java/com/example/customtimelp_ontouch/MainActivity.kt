b8041cf04b61c3f6f9a76afd8e84066e48bd34d4
Aashay <Aashay@Aashays-MacBook-Air.local>
2020-09-23 14:35:21 +0530

    Initial commit

---
+++ app/src/main/java/com/example/customtimelp_ontouch/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.customtimelp_ontouch
+
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val view = findViewById<TextView>(R.id.tvView)
+
+        view.setOnClickListener(object : DoubleClickListener() {
+            override fun onDoubleClick(v: View?) {
+                Toast.makeText(applicationContext,"Double Click",Toast.LENGTH_SHORT).show()
+            }
+        })
+
+
+    }
+
+    abstract class DoubleClickListener : View.OnClickListener {
+        var lastClickTime: Long = 0
+        override fun onClick(v: View?) {
+            val clickTime = System.currentTimeMillis()
+            if (clickTime - lastClickTime < DOUBLE_CLICK_TIME_DELTA) {
+                onDoubleClick(v)
+            }
+            lastClickTime = clickTime
+        }
+
+        abstract fun onDoubleClick(v: View?)
+
+        companion object {
+            private const val DOUBLE_CLICK_TIME_DELTA: Long = 300 //milliseconds
+        }
+    }
+}
